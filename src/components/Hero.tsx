4db4bc357a12adf41c32b332900838650a23dba3
Nihal Singh <31930532+nihal197@users.noreply.github.com>
2025-03-21 06:30:01 +0530

    added Heart Emoji

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -53,7 +53,7 @@ export function Hero() {
                   strokeWidth={4}
                   className="text-ali absolute -bottom-5 -right-5 h-10 w-10"
                 />
-                Your complete platform for the Design.
+                Me + You = ❤️
               </h1>
               <div className="flex items-center justify-center gap-1">
                 <span className="relative flex h-3 w-3 items-center justify-center">
@@ -66,21 +66,21 @@ export function Hero() {
           </div>
 
           <h1 className="mt-8 text-2xl md:text-2xl">
-            Welcome to my creative playground! I&#39;m{" "}
-            <span className="text-ali font-bold">Ali </span>
+            Welcome to our creative playground! I&#39;m{" "}
+            <span className="text-ali font-bold">ME </span>
           </h1>
 
           <p className="md:text-md mx-auto mb-16 mt-2 max-w-2xl px-6 text-sm text-primary/60 sm:px-6 md:max-w-4xl md:px-20 lg:text-lg">
-            I craft enchanting visuals for brands, and conjure design resources
+            We craft enchanting visuals for brands, and conjure design resources
             to empower others.
           </p>
           <div className="flex justify-center gap-2">
-            <Link to={"/dashboard"}>
+            {/* <Link to={"/dashboard"}>
               <Button variant="default" size="lg">
                 Start Project
               </Button>
-            </Link>
-            <a href={"https://cal.com/aliimam/designali"} target="_blank">
+            </Link> */}
+            <a href={"/#contact"} target="_blank">
               <Button variant="outline" size="lg">
                 Book a call
               </Button>
